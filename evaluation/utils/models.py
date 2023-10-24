80be3f6fc0cac1ad842bca7b597cf63766900a37
abigailhayes <abigail.joy.hayes@gmail.com>
2023-10-24 13:35:59 +0200

    Basic utils files for SEAT evaluation

---
+++ evaluation/utils/models.py
@@ -0,0 +1,10 @@
+# A portion from models.py in https://github.com/McGill-NLP/bias-bench
+
+from functools import partial
+
+import torch
+import transformers
+
+class RobertaModel:
+    def __new__(self, model_name_or_path):
+        return transformers.RobertaModel.from_pretrained(model_name_or_path))
