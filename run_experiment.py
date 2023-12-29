f4b69a8665021e855e2dfe3c5c67f3264dbc9df2
Jana Jung <jana@dr-a-jung.de>
2023-12-29 16:56:47 +0100

    structure of pipeline, following run_glue.py approach

--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,34 @@
-from training.model_import import Model
+# imports
+import transformers
+from dataclasses import dataclass, field
 
-model = Model(model_path="training/models/MNLI_model")
 
-print(model)
+# dataclass that contains all arguments needed to run the experiment
+@dataclass
+class ExperimentArguments:
+
+
+
+
+
+# main function that runs the pipeline (evaluations and pruning dependent on arguments)
+def main():
+    # load model
+    model = transformers.RobertaForSequenceClassification.from_pretrained(
+        "models/MNLI_model/",
+        use_safetensors=True,
+        local_files_only=True
+    )
+
+
+    # evaluation 1
+
+
+    # pruning
+
+
+    # evaluation 2
+
+
+if __name__ == "__main__":
+    main()
