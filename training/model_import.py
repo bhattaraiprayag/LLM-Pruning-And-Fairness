03997ca0b7ef8db3e267f5b23bc92c0100122b68
Jana Jung <jana@dr-a-jung.de>
2023-12-29 16:13:52 +0100

    Revert "introduce model class"
    This reverts commit 66c86023df8841ecbaa7126b2c8e9ff788b06525.

--- training/model_import.py
+++ training/model_import.py
@@ -1,6 +1,5 @@
 import transformers
 
-class Model:
-    def __new__(self, model_path):
-        return transformers.RobertaForSequenceClassification.from_pretrained(model_path, use_safetensors=True, local_files_only=True)
+model = transformers.RobertaForSequenceClassification.from_pretrained("models/MNLI_model/", use_safetensors=True, local_files_only=True)
 
+print(model)
