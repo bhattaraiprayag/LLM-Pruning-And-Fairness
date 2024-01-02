c9e658013cdee9d501c5cc88bb931e7009cb590e
Jana Jung <jana@dr-a-jung.de>
2024-01-02 12:28:35 +0100

    added variables

--- results/create_results_df.py
+++ results/create_results_df.py
@@ -6,17 +6,21 @@ results_df = pd.DataFrame(columns=['ID',
                                    'date',
                                    'seed',
                                    'task',  # MNLI or STS-B
-                                   'pruning_method',
+                                   'pruning_method', # None if using base model
+                                   'sparsity_level'
                                    'SEAT_before',
                                    'SEAT_after',
                                    'WEAT_before',
                                    'WEAT_after',
                                    'StereoSet_before',
                                    'StereoSet_after',
-                                   'BiasNLI_before',
-                                   'BiasNLI_after',
+                                   'BiasNLI_NN_before',
+                                   'BiasNLI_NN_after',
+                                   'BiasNLI_FN_before',
+                                   'BiasNLI_FN_after',
                                    'BiasSTS_before',
                                    'BiasSTS_after',
+                                   'model_performance'
                                    # ??? pruning variables
                                    ])
 
