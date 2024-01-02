cc90cb17a90a8bc4ed8be338dec9806cbc38515c
abigailhayes <abigail.joy.hayes@gmail.com>
2024-01-02 14:22:28 +0000

    Updated to be more efficient

--- evaluation/bias_nli.py
+++ evaluation/bias_nli.py
@@ -14,23 +14,15 @@ pipe = transformers.TextClassificationPipeline(model=model, tokenizer=tokenizer,
 # Load csv with test sentences, then make model predictions for each
 with open(f'{thisdir}/evaluation/data/bias_nli/occupation_gender.csv', mode='r') as csv_file:
     test_dict = csv.DictReader(csv_file)
-    line_count = 0
-    nn_count = 0
-    fn_count = 0
+    pair_list = []
+    # Get all premise/hypothesis pairs in a list
     for row in test_dict:
-        line_count += 1
-        if line_count % 1000 == 0:
-            print(line_count)
-        prediction = pipe((row["premise"],row["hypothesis"]))
-        ent = [i['score'] for i in prediction if i['label']=='entailment'][0]
-        neut = [i['score'] for i in prediction if i['label']=='neutral'][0]
-        con = [i['score'] for i in prediction if i['label']=='contradiction'][0]
-        nn_count += neut
-        if neut == max(ent, neut, con):
-            fn_count += 1
+        pair_list.append((row["premise"],row["hypothesis"]))
+    # Make predictions with model
+    prediction = pipe(pair_list)
     results = {}
-    results['fraction_neutral'] = fn_count/line_count # Fraction of sentences rated as neutral - perfect would be all
-    results['net_neutral'] = nn_count / line_count # Average probability given to neutrality - perfect would be 1
+    results['net_neutral'] = sum([next((item.get('score') for item in i if item["label"] == "neutral"), False) for i in prediction])/len(pair_list)
+    results['fraction_neutral'] = sum([1 for i in prediction if max(i, key=lambda x:x['score'])['label']=='neutral'])/len(pair_list)
     os.makedirs(f"{thisdir}/evaluation/results/bias_nli", exist_ok=True)
     with open(f"{thisdir}/evaluation/results/bias_nli/{experiment_id}.json", "w") as f:
         json.dump(results, f)
