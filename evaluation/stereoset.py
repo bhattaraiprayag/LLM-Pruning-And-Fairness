a7dceb1e42453a496aae8675076e0131dc3466cf
abigailhayes <abigail.joy.hayes@gmail.com>
2024-01-08 16:01:57 +0000

    Final tweaks to StereoSet

--- evaluation/stereoset.py
+++ evaluation/stereoset.py
@@ -1,25 +1,25 @@
 # Code amended from https://github.com/luohongyin/ESP/blob/main/eval_stereo.py
 
 import json
+import os
 
 import evaluation.utils.stereoset as ss
 
 def stereoset(model, tokenizer, exp_id):
 
+    # Setup
     eval_split = 'intrasentence' # More commonly used, but can also get intersentence
     cls_head = int(0) # Can be 0, 1 or 2 for entailment, neutral or contradiction
     eval_mode = 'score'
 
+    # Get data
     sent_list, data = ss.load_data('evaluation/data/StereoSet/test.json', eval_split)
 
+    # Get output values
     score_board, pred_board = ss.cls_evaluate(tokenizer, model, cls_head, sent_list, batch_size=4)
 
+    # Generate scores
     bias_type_list = ['gender', 'profession', 'race', 'religion', 'overall']
-
-    print('=========================================================')
-    print(f'============== Exp of cls_head: {cls_head} ==============')
-    print('=========================================================')
-
     overall = {}
 
     for bias_type in bias_type_list:
@@ -31,15 +31,13 @@ def stereoset(model, tokenizer, exp_id):
             bias_type=bias_type, input_type=eval_mode
         )
 
-        print(f'LM score = {lm_score}')
-        print(f'SS score = {stereo_score}')
-        print(f'iCat score = {icat_score}')
         working['LM'] = lm_score
         working['SS'] = stereo_score
         working['iCAT'] = icat_score
         overall[bias_type] = working
 
     # Save whole output file
+    os.makedirs(f'results/{exp_id}', exist_ok=True)
     with open(f'results/{exp_id}/stereoset.json', 'w') as f:
         json.dump(overall, f, indent=2)
 
