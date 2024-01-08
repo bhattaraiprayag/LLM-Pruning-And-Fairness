acd36386bf860b70b9abf8e89d7aada6593f7fcc
abigailhayes <abigail.joy.hayes@gmail.com>
2024-01-08 10:54:29 +0000

    Add score calculation step

--- evaluation/stereoset_all.py
+++ evaluation/stereoset_all.py
@@ -1,15 +1,22 @@
-# Adapted from experiments/stereoset.py in https://github.com/McGill-NLP/bias-bench
+# Adapted from StereoSet processing in https://github.com/McGill-NLP/bias-bench
 
 import argparse
+from collections import Counter, OrderedDict, defaultdict
+import glob
 import json
 import os
+import re
 
 import transformers
+import numpy as np
 
 from utils.stereoset import StereoSetRunner
 import utils.models as models
 from utils.experiment_id import generate_experiment_id
 from utils.model_utils import _is_generative
+from utils.stereoset import dataloader
+import utils.stereoset_stats as ss
+
 
 def stereoset(model, tokenizer, exp_id):
 
@@ -25,5 +32,11 @@ def stereoset(model, tokenizer, exp_id):
 
     # Save intermediate result
     os.makedirs(f'results/{exp_id}', exist_ok=True)
-    with open(f"results/{exp_id}/stereoset.json", "w") as f:
+    with open(f"results/{exp_id}/stereoset_raw.json", "w") as f:
         json.dump(results, f, indent=2)
+
+    # Getting actual score
+    overall = parse_file(f"evaluation/data/stereoset/test.json", f"results/{exp_id}/stereoset_raw.json")
+
+    with open(f"results/{exp_id}/stereoset_results.json", "w+") as f:
+        json.dump(overall, f, indent=2)
