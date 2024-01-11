b296d34ec7b1f35a2a59043b94108264ab98a00d
Jana Jung <jana@dr-a-jung.de>
2024-01-11 06:57:18 +0100

    deleted unused input statements

--- evaluation/seat.py
+++ evaluation/seat.py
@@ -5,10 +5,6 @@ import os
 
 from evaluation.utils.seat import SEATRunner
 
-# TO DO: delete unused files
-#import utils.models as models
-#from utils.experiment_id import generate_experiment_id
-
 
 def seatandweat(model, tokenizer, exp_id, seed):
     runner = SEATRunner(
@@ -20,11 +16,10 @@ def seatandweat(model, tokenizer, exp_id, seed):
         tokenizer=tokenizer,
         seed=seed
     )
-    results = runner()
-    print(results)
+    all_results = runner()
 
     # TO DO: return most important results (which are the most important?)
 
     os.makedirs(f'results/{exp_id}', exist_ok=True) # maybe save as csv instead?
     with open(f'results/{exp_id}/seatandweat.json', 'w') as file:
-        json.dump(results, file)
+        json.dump(all_results, file)
