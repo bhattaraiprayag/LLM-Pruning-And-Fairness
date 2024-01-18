1a13dafcada5b3dd6000021d9af852dde9b15f85
abigailhayes <abigail.joy.hayes@gmail.com>
2024-01-18 11:34:51 +0000

    Basic plot for performance vs bias measure

--- report/plots.py
+++ report/plots.py
@@ -13,6 +13,7 @@ def results_filter(results, task, pruning_method):
 
 # Create a plot where all the pruning methods are included for some metric, changing over sparsity levels
 def all_prune_figure(results, task, measure, values=[0,1,0.99]):
+    # The first 2 entries of values set the y-axis, the other is for the optimum to add a green line
     pruning_methods = ['l1-unstructured', 'l1-unstructured-linear']
     for pm in pruning_methods:
         data = results_filter(results, task, pm)
@@ -25,6 +26,18 @@ def all_prune_figure(results, task, measure, values=[0,1,0.99]):
     plt.savefig(f'./report/figures/all_prune_{task}_{measure}.png')
     plt.close()
 
+def all_performance_figure(results, task, measure, values=[0,1,0.99]):
+    groups = results.groupby('pruning_method')
+    for name, group in groups:
+        plt.plot(group['Matched Acc'], group[measure], marker='o', linestyle='', markersize=12, label=name)
+    plt.title(f'{task} - {measure}')
+    plt.xlabel("Matched accuracy")
+    plt.legend()
+    plt.ylim(values[0], values[1])
+    plt.axhline(y=values[2], color='lightgreen', linestyle='dashed')
+    plt.savefig(f'./report/figures/all_performance_{task}_{measure}.png')
+    plt.close()
+
 def all_plots(results):
     all_prune_figure(results, 'mnli', 'seat_gender', [0,1,0.01])
     all_prune_figure(results, 'mnli', 'weat_gender', [0,1,0.01])
@@ -32,3 +45,4 @@ def all_plots(results):
     all_prune_figure(results, 'mnli', 'Stereoset_SS_gender', [0,1,0.5])
     all_prune_figure(results, 'mnli', 'Matched Acc')
     all_prune_figure(results, 'mnli', 'Mismatched Acc')
+    all_performance_figure(results, 'mnli', 'seat_gender', [0,1,0.01])
