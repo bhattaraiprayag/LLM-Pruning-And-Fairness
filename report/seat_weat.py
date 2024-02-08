bbb74dfd04f7fb194ade0514919c0a75ab72b7b2
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-08 19:01:20 +0100

    Split to use utils file and removed testing ammendment

--- report/seat_weat.py
+++ report/seat_weat.py
@@ -1,19 +1,12 @@
 import json
-import pandas as pd
-
-def get_id_info(filepath):
-    # Read in current results data frame
-    results_df = pd.read_csv(filepath)
-    # Keep just the columns of interest
-    new_df = results_df[['ID', 'date', 'device', 'seed', 'task', 'pruning_method', 'sparsity_level', 'temperature']]
-    return new_df
+import utils
 
 def main():
-    new_df = get_id_info('results/results.csv')
+    new_df = utils.get_id_info('results/results.csv')
 
     SEAT_gender, SEAT_race, SEAT_illness, SEAT_religion, WEAT_gender, WEAT_race, WEAT_illness = ([] for i in range(7))
 
-    for i in [1]: #new_df['ID']:
+    for i in new_df['ID']:
         with open(f'results/run{i}/seatandweat_aggregated.json', 'r') as f:
             json_file = json.load(f)
             SEAT_gender.append(json_file['SEAT_gender'])
