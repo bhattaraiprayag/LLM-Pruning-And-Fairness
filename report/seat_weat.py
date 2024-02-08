ae41709f0a5b88ef6e8a6eac60db3a33ac19f552
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-08 18:47:23 +0100

    Creates SEAT and WEAT csv output, summarising all runs

--- report/seat_weat.py
+++ report/seat_weat.py
@@ -1,4 +1,3 @@
-import os
 import json
 import pandas as pd
 
@@ -12,11 +11,28 @@ def get_id_info(filepath):
 def main():
     new_df = get_id_info('results/results.csv')
 
-    for i in new_df['ID']:
+    SEAT_gender, SEAT_race, SEAT_illness, SEAT_religion, WEAT_gender, WEAT_race, WEAT_illness = ([] for i in range(7))
+
+    for i in [1]: #new_df['ID']:
         with open(f'results/run{i}/seatandweat_aggregated.json', 'r') as f:
             json_file = json.load(f)
-        print(i)
+            SEAT_gender.append(json_file['SEAT_gender'])
+            SEAT_race.append(json_file['SEAT_race'])
+            SEAT_illness.append(json_file['SEAT_illness'])
+            SEAT_religion.append(json_file['SEAT_religion'])
+            WEAT_gender.append(json_file['WEAT_gender'])
+            WEAT_race.append(json_file['WEAT_race'])
+            WEAT_illness.append(json_file['WEAT_illness'])
 
+    new_df = new_df.assign(seat_gender = SEAT_gender,
+                           seat_race = SEAT_race,
+                           seat_illness = SEAT_illness,
+                           seat_religion = SEAT_religion,
+                           weat_gender = WEAT_gender,
+                           weat_race = WEAT_race,
+                           weat_illness = WEAT_illness)
+
+    new_df.to_csv('results/results_seat_weat.csv', index=False)
 
 if __name__ == "__main__":
     main()
