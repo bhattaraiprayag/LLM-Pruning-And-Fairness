5c2d63c5f92bd595424a1e5aa0cb287eb0b9d600
Jana Jung <jana@dr-a-jung.de>
2024-02-16 16:42:15 +0100

    move functions to utils so that only structured_pruning function remains

--- pruning/structured_pruning.py
+++ pruning/structured_pruning.py
@@ -1,73 +1,16 @@
 # Based on https://github.com/huggingface/transformers/blob/main/examples/research_projects/bertology/run_bertology.py
 
-import argparse
 import logging
-import os
-from datetime import datetime
-import numpy as np
 import torch
 from torch.utils.data import DataLoader, SequentialSampler, Subset
 from torch.utils.data.distributed import DistributedSampler
-from utils import load_examples, get_seed, compute_heads_importance, mask_heads
+from utils import load_examples, get_seed, mask_heads, prune_heads
 from transformers import glue_processors as processors
 from transformers import glue_output_modes as output_modes
-from utils import glue_compute_metrics as compute_metrics
 from utils import get_device
 
 logger = logging.getLogger(__name__)
 logging.getLogger("experiment_impact_tracker.compute_tracker.ImpactTracker").disabled = True
-
-
-
-# Pruning involves removing the weights of masked attention heads, effectively reducing the model's size and
-# complexity. compares the performance of the pruned model to the masked model on the evaluation dataset. compares
-# the execution time of the two models to assess the impact of pruning on computational efficiency.
-def prune_heads(model, eval_dataloader, device, local_rank, output_dir, task, head_mask):
-    """ This method shows how to prune head (remove heads weights) based on
-        the head importance scores as described in Michel et al. (http://arxiv.org/abs/1905.10650)
-    """
-    metric_name = {
-        "mnli": "acc",
-        "sts-b": "corr",
-    }[task]
-    output_mode = output_modes[task]
-
-    # Try pruning and test time speedup
-    # Pruning is like masking but we actually remove the masked weights
-    before_time = datetime.now()
-    _, _, preds, labels = compute_heads_importance(
-        model, eval_dataloader, device, local_rank, output_dir, compute_importance=False, head_mask=head_mask
-    )
-    preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
-    score_masking = compute_metrics(task, preds, labels)[metric_name]
-    original_time = datetime.now() - before_time
-
-    original_num_params = sum(p.numel() for p in model.parameters())
-    heads_to_prune = {}
-    for layer in range(len(head_mask)):
-        heads_to_mask = [h[0] for h in (1 - head_mask[layer].long()).nonzero().tolist()]
-        heads_to_prune[layer] = heads_to_mask
-    assert sum(len(h) for h in heads_to_prune.values()) == (1 - head_mask.long()).sum().item()
-    logger.info(f"{heads_to_prune}")
-    model.prune_heads(heads_to_prune)
-    pruned_num_params = sum(p.numel() for p in model.parameters())
-
-    before_time = datetime.now()
-    _, _, preds, labels = compute_heads_importance(
-        model, eval_dataloader, device, local_rank, output_dir, compute_importance=False, head_mask=None
-    )
-    preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
-    score_pruning = compute_metrics(task, preds, labels)[metric_name]
-    new_time = datetime.now() - before_time
-
-    logger.info(
-        "Pruning: original num of params: %.2e, after pruning %.2e (%.1f percents)",
-        original_num_params,
-        pruned_num_params,
-        pruned_num_params / original_num_params * 100,
-    )
-    logger.info("Pruning: score with masking: %f score with pruning: %f", score_masking, score_pruning)
-    logger.info("Pruning: speed ratio (new timing / original timing): %f percents", original_time / new_time * 100)
 
 
 def structured_pruning(model, tokenizer, seed, task, device, masking_amount, masking_threshold):
--- pruning/utils.py
+++ pruning/utils.py
@@ -2,9 +2,10 @@ import os
 import logging
 import re
 import random
+from datetime import datetime
 import numpy as np
 import torch
-import torch.nn as nn
+from tqdm import tqdm  # display progress bars during long-running operations
 from sklearn.metrics import matthews_corrcoef
 from torch import threshold
 from transformers import RobertaModel, RobertaConfig
@@ -17,7 +18,7 @@ from transformers import glue_convert_examples_to_features as convert_examples_t
 
 ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP = {
     'roberta-base': "https://s3.amazonaws.com/models.huggingface.co/bert/roberta-base-pytorch_model.bin"
-    }
+}
 
 
 def get_seed(seed):
@@ -51,6 +52,211 @@ RobertaLayerNorm = torch.nn.LayerNorm
 
 # Logger Setup
 logger = logging.getLogger(__name__)
+
+
+def print_2d_tensor(tensor):
+    """ Print a 2D tensor (used to log and visualize information about attention heads)"""
+    logger.info("lv, h >\t" + "\t".join(f"{x + 1}" for x in range(len(tensor))))
+    for row in range(len(tensor)):
+        if tensor.dtype != torch.long:
+            logger.info(f"layer {row + 1}:\t" + "\t".join(f"{x:.5f}" for x in tensor[row].cpu().data))
+        else:
+            logger.info(f"layer {row + 1}:\t" + "\t".join(f"{x:d}" for x in tensor[row].cpu().data))
+
+
+def compute_heads_importance(model, eval_dataloader, device, local_rank, output_dir, compute_importance=True,
+                             head_mask=None):
+    """ This method shows how to compute
+        head importance scores according to http://arxiv.org/abs/1905.10650
+    """
+    # Prepare our tensors
+    n_layers, n_heads = model.roberta.config.num_hidden_layers, model.roberta.config.num_attention_heads
+    head_importance = torch.zeros(n_layers, n_heads).to(device)
+    attn_entropy = torch.zeros(n_layers, n_heads).to(device)
+
+    if head_mask is None and compute_importance:
+        head_mask = torch.ones(n_layers, n_heads).to(device)
+        head_mask.requires_grad_(requires_grad=True)
+    elif compute_importance:
+        head_mask = head_mask.clone().detach()
+        head_mask.requires_grad_(requires_grad=True)
+
+    preds = None
+    labels = None
+    tot_tokens = 0.0
+    for step, batch in enumerate(tqdm(eval_dataloader, desc="Iteration", disable=local_rank not in [-1, 0])):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, input_mask, segment_ids, label_ids = batch
+
+        # Do a forward pass (not with torch.no_grad() since we need gradients for importance score - see below)
+        outputs = model(
+            input_ids, token_type_ids=segment_ids, attention_mask=input_mask, labels=label_ids, head_mask=head_mask
+        )
+        loss, logits, all_attentions = (
+            outputs[0],
+            outputs[1],
+            outputs[-1],
+        )  # Loss and logits are the first, attention the last
+        loss.backward()  # Backpropagate to populate the gradients in the head mask
+
+        # compute head importance
+        if compute_importance:
+            head_importance += head_mask.grad.abs().detach()
+            head_mask.grad.zero_()  # set gradients to zero to not overestimate importance of early batches
+
+        # Also store our logits/labels if we want to compute metrics afterwards
+        if preds is None:
+            preds = logits.detach().cpu().numpy()
+            labels = label_ids.detach().cpu().numpy()
+        else:
+            preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+            labels = np.append(labels, label_ids.detach().cpu().numpy(), axis=0)
+
+        tot_tokens += input_mask.float().detach().sum().data
+
+    if compute_importance:
+        # Normalize
+        head_importance /= tot_tokens
+        # Layerwise importance normalization
+        if not args.dont_normalize_importance_by_layer:
+            exponent = 2
+            norm_by_layer = torch.pow(torch.pow(head_importance, exponent).sum(-1), 1 / exponent)
+            head_importance /= norm_by_layer.unsqueeze(-1) + 1e-20
+
+        if not args.dont_normalize_global_importance:
+            head_importance = (head_importance - head_importance.min()) / (
+                    head_importance.max() - head_importance.min())
+
+        # Print/save matrices
+        np.save(os.path.join(output_dir, "head_importance.npy"), head_importance.detach().cpu().numpy())
+
+        logger.info("Head importance scores")
+        print_2d_tensor(head_importance)
+        logger.info("Head ranked by importance scores")
+        head_ranks = torch.zeros(head_importance.numel(), dtype=torch.long, device=device)
+        head_ranks[head_importance.view(-1).sort(descending=True)[1]] = torch.arange(
+            head_importance.numel(), device=device
+        )
+        head_ranks = head_ranks.view_as(head_importance)
+        print_2d_tensor(head_ranks)
+
+    return attn_entropy, head_importance, preds, labels
+
+
+def mask_heads(model, eval_dataloader, device, local_rank, output_dir, task, masking_amount, masking_threshold):
+    """ This method shows how to mask head (set some heads to zero), to test the effect on the network,
+        based on the head importance scores, as described in Michel et al. (http://arxiv.org/abs/1905.10650)
+    """
+    metric_name = {
+        "mnli": "acc",
+        "sts-b": "corr",
+    }[task]
+    output_mode = output_modes[task]
+
+    _, head_importance, preds, labels = compute_heads_importance(model, eval_dataloader, device, local_rank, output_dir)
+    preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
+    original_score = glue_compute_metrics(task, preds, labels)[metric_name]
+    logger.info("Pruning: original score: %f, threshold: %f", original_score, original_score * masking_threshold)
+
+    new_head_mask = torch.ones_like(head_importance)
+    num_to_mask = max(1, int(new_head_mask.numel() * masking_amount))
+
+    current_score = original_score
+    i = 0
+    while current_score >= original_score * masking_threshold:
+        head_mask = new_head_mask.clone()  # save current head mask
+        i += 1
+        # heads from least important to most - keep only not-masked heads
+        head_importance[head_mask == 0.0] = float("Inf")
+        current_heads_to_mask = head_importance.view(-1).sort()[1]
+
+        if len(current_heads_to_mask) <= num_to_mask:
+            break
+
+        # mask heads
+        selected_heads_to_mask = []
+        for head in current_heads_to_mask:
+            if len(selected_heads_to_mask) == num_to_mask or head_importance.view(-1)[head.item()] == float("Inf"):
+                break
+            layer_idx = head.item() // model.roberta.config.num_attention_heads
+            head_idx = head.item() % model.roberta.config.num_attention_heads
+            new_head_mask[layer_idx][head_idx] = 0.0
+            selected_heads_to_mask.append(head.item())
+
+        if not selected_heads_to_mask:
+            break
+
+        logger.info("Heads to mask: %s", str(selected_heads_to_mask))
+
+        # new_head_mask = new_head_mask.view_as(head_mask)
+        print_2d_tensor(new_head_mask)
+
+        # Compute metric and head importance again
+        _, head_importance, preds, labels = compute_heads_importance(
+            model, eval_dataloader, device, local_rank, output_dir, head_mask=new_head_mask
+        )
+        preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
+        current_score = glue_compute_metrics(task, preds, labels)[metric_name]
+        logger.info(
+            "Masking: current score: %f, remaning heads %d (%.1f percents)",
+            current_score,
+            new_head_mask.sum(),
+            new_head_mask.sum() / new_head_mask.numel() * 100,
+        )
+
+    logger.info("Final head mask")
+    print_2d_tensor(head_mask)
+    np.save(os.path.join(output_dir, "head_mask.npy"), head_mask.detach().cpu().numpy())
+
+    return head_mask
+
+
+def prune_heads(model, eval_dataloader, device, local_rank, output_dir, task, head_mask):
+    """ This method shows how to prune head (remove heads weights) based on
+        the head importance scores as described in Michel et al. (http://arxiv.org/abs/1905.10650)
+    """
+    metric_name = {
+        "mnli": "acc",
+        "sts-b": "corr",
+    }[task]
+    output_mode = output_modes[task]
+
+    # Try pruning and test time speedup
+    # Pruning is like masking but we actually remove the masked weights
+    before_time = datetime.now()
+    _, _, preds, labels = compute_heads_importance(
+        model, eval_dataloader, device, local_rank, output_dir, compute_importance=False, head_mask=head_mask
+    )
+    preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
+    score_masking = glue_compute_metrics(task, preds, labels)[metric_name]
+    original_time = datetime.now() - before_time
+
+    original_num_params = sum(p.numel() for p in model.parameters())
+    heads_to_prune = {}
+    for layer in range(len(head_mask)):
+        heads_to_mask = [h[0] for h in (1 - head_mask[layer].long()).nonzero().tolist()]
+        heads_to_prune[layer] = heads_to_mask
+    assert sum(len(h) for h in heads_to_prune.values()) == (1 - head_mask.long()).sum().item()
+    logger.info(f"{heads_to_prune}")
+    model.prune_heads(heads_to_prune)
+    pruned_num_params = sum(p.numel() for p in model.parameters())
+
+    before_time = datetime.now()
+    _, _, preds, labels = compute_heads_importance(
+        model, eval_dataloader, device, local_rank, output_dir, compute_importance=False, head_mask=None
+    )
+    preds = np.argmax(preds, axis=1) if output_mode == "classification" else np.squeeze(preds)
+    score_pruning = glue_compute_metrics(task, preds, labels)[metric_name]
+    new_time = datetime.now() - before_time
+
+    logger.info(
+        "Pruning: original num of params: %.2e, after pruning %.2e (%.1f percents)",
+        original_num_params,
+        pruned_num_params,
+        pruned_num_params / original_num_params * 100,
+    )
+    logger.info("Pruning: score with masking: %f score with pruning: %f", score_masking, score_pruning)
+    logger.info("Pruning: speed ratio (new timing / original timing): %f percents", original_time / new_time * 100)
 
 
 def load_examples(task, tokenizer, data_dir):
