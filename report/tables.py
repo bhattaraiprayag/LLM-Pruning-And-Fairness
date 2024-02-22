e9b27a40ec29ef47118f2e42ca50d87c4f778e3b
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-22 11:04:52 +0100

    Note of error in data

--- report/tables.py
+++ report/tables.py
@@ -9,7 +9,7 @@ data_bnli = pd.read_csv('results/run1/bias_nli.csv')
 group_bnli = data_bnli.groupby(['premise_filler_word', 'hypothesis_filler_word'], as_index=False)[
     ['entailment', 'neutral', 'contradiction']].mean()
 
-# Keep most extreme values for each gendered word
-entail_bnli = group_bnli.sort_values('entailment', ascending=False).drop_duplicates(['hypothesis_filler_word'])
-contradict_bnli = group_bnli.sort_values('contradiction', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+# Keep most extreme values for each gendered word - NOTE CONTRADICTION AND ENTAILMENT CURRENTLY SAVED THE WRONG WAY AROUND
+entail_bnli = group_bnli.sort_values('contradiction', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+contradict_bnli = group_bnli.sort_values('entailment', ascending=False).drop_duplicates(['hypothesis_filler_word'])
 neutral_bnli = group_bnli.sort_values('neutral', ascending=False).drop_duplicates(['hypothesis_filler_word'])
