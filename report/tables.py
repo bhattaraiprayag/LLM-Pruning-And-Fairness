d7ad64acce99ac7df7f5b6a75edfe036747823aa
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-22 10:50:02 +0100

    Group for each job-gendered word pair

--- report/tables.py
+++ report/tables.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 # Table for Bias NLI base model analysis
 data_bnli = pd.read_csv('results/run1/bias_nli.csv')
+
+group_bnli = data_bnli.groupby(['premise_filler_word', 'hypothesis_filler_word'], as_index=False)[['entailment', 'neutral', 'contradiction']].mean()
