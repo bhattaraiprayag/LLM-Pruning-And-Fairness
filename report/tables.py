b87fb0f8f19a0a27f34d241d45b95ef99edc0320
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-22 11:59:22 +0100

    Function with run number

--- report/tables.py
+++ report/tables.py
@@ -2,29 +2,31 @@ import pandas as pd
 
 ### Table for Bias NLI base model analysis
 
-# Load data
-data_bnli = pd.read_csv('results/run1/bias_nli.csv')
-# Group by premise-hypothesis pairs
-group_bnli = data_bnli.groupby(['premise_filler_word', 'hypothesis_filler_word'], as_index=False)[
-    ['entailment', 'neutral', 'contradiction']].mean()
+def bnli_table(run_no):
 
-# Keep most extreme values for each gendered word - NOTE CONTRADICTION AND ENTAILMENT CURRENTLY SAVED THE WRONG WAY AROUND
-entail_bnli = group_bnli.sort_values('contradiction', ascending=False).drop_duplicates(['hypothesis_filler_word'])
-contradict_bnli = group_bnli.sort_values('entailment', ascending=False).drop_duplicates(['hypothesis_filler_word'])
-neutral_bnli = group_bnli.sort_values('neutral', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+    # Load data
+    data_bnli = pd.read_csv(f'results/run{run_no}/bias_nli.csv')
+    # Group by premise-hypothesis pairs
+    group_bnli = data_bnli.groupby(['premise_filler_word', 'hypothesis_filler_word'], as_index=False)[
+        ['entailment', 'neutral', 'contradiction']].mean()
 
-# Final table
-output_bnli = entail_bnli[['hypothesis_filler_word', 'premise_filler_word']].copy()
-output_bnli.rename(columns={'premise_filler_word': 'Entailment'}, inplace=True)
-output_bnli = output_bnli.merge(neutral_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
-output_bnli.rename(columns={'premise_filler_word': 'Neutral'}, inplace=True)
-output_bnli = output_bnli.merge(contradict_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
-output_bnli.rename(columns={'premise_filler_word': 'Contradiction', 'hypothesis_filler_word':'Gendered word'}, inplace=True)
+    # Keep most extreme values for each gendered word - NOTE CONTRADICTION AND ENTAILMENT CURRENTLY SAVED THE WRONG WAY AROUND
+    entail_bnli = group_bnli.sort_values('contradiction', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+    contradict_bnli = group_bnli.sort_values('entailment', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+    neutral_bnli = group_bnli.sort_values('neutral', ascending=False).drop_duplicates(['hypothesis_filler_word'])
 
-latex = output_bnli.to_latex(index=False,
-                             column_format='lccc',
-                             label='tab:bnli',
-                             caption=f'Results from Bias-NLI for the job titles that most entail or contradict gendered words.')
-# Save the LaTeX output
-with open("report/tables/test.tex", "w") as f:
-    f.write(latex)
+    # Final table
+    output_bnli = entail_bnli[['hypothesis_filler_word', 'premise_filler_word']].copy()
+    output_bnli.rename(columns={'premise_filler_word': 'Entailment'}, inplace=True)
+    output_bnli = output_bnli.merge(neutral_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
+    output_bnli.rename(columns={'premise_filler_word': 'Neutral'}, inplace=True)
+    output_bnli = output_bnli.merge(contradict_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
+    output_bnli.rename(columns={'premise_filler_word': 'Contradiction', 'hypothesis_filler_word':'Gendered word'}, inplace=True)
+
+    latex = output_bnli.to_latex(index=False,
+                                 column_format='lccc',
+                                 label=f'tab:bnli{run_no}',
+                                 caption=f'Results from Bias-NLI for the job titles that most entail or contradict gendered words.')
+    # Save the LaTeX output
+    with open(f"report/tables/bnli{run_no}.tex", "w") as f:
+        f.write(latex)
