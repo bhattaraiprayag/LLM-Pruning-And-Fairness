8d68c2c6d49ab9a1399621d7f8f1dfac13dd45e6
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-22 11:22:03 +0100

    Compile into a single table

--- report/tables.py
+++ report/tables.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 ### Table for Bias NLI base model analysis
@@ -13,3 +12,11 @@ group_bnli = data_bnli.groupby(['premise_filler_word', 'hypothesis_filler_word']
 entail_bnli = group_bnli.sort_values('contradiction', ascending=False).drop_duplicates(['hypothesis_filler_word'])
 contradict_bnli = group_bnli.sort_values('entailment', ascending=False).drop_duplicates(['hypothesis_filler_word'])
 neutral_bnli = group_bnli.sort_values('neutral', ascending=False).drop_duplicates(['hypothesis_filler_word'])
+
+# Final table
+output_bnli = entail_bnli[['hypothesis_filler_word', 'premise_filler_word']].copy()
+output_bnli.rename(columns={'premise_filler_word': 'Entailment'}, inplace=True)
+output_bnli = output_bnli.merge(neutral_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
+output_bnli.rename(columns={'premise_filler_word': 'Neutral'}, inplace=True)
+output_bnli = output_bnli.merge(contradict_bnli[['hypothesis_filler_word', 'premise_filler_word']], how='left', on='hypothesis_filler_word')
+output_bnli.rename(columns={'premise_filler_word': 'Contradiction', 'hypothesis_filler_word':'Gendered word'}, inplace=True)
