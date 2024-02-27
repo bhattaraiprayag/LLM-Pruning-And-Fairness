c1280182db795442eff552d72c588a7d1009bc8d
abigailhayes <abigail.joy.hayes@gmail.com>
2024-02-27 11:24:05 +0100

    Add stsb comparison plot

--- report/plots.R
+++ report/plots.R
@@ -222,6 +222,32 @@ perf_stsb <- function(data, pruning_method_set, base_folder){
          units = "px")
 }
 
+perf_stsb_compare <- function(data, base_folder){
+  working <- data %>%
+    filter(task=='stsb') %>%
+    group_by(pruning_method, sparsity) %>%
+    summarise(spearmanr = mean(Spearmanr))
+  
+  output <-
+    ggplot(working, aes(x=sparsity, y=spearmanr, group=pruning_method, colour=pruning_method)) +
+    geom_line(linewidth=2) +
+    scale_fill_manual(values=colours) +
+    scale_colour_manual(values=colours) +
+    scale_x_continuous(expand = c(0,0),
+                       limits = c(0,1)) +
+    scale_y_continuous(expand = c(0,0),
+                       limits = c(NA,1)) +
+    theme_bw() + 
+    guides(colour=guide_legend(title='Metric:')) +
+    coord_cartesian(clip='off')
+  
+  ggsave(filename = paste0(base_folder, 'LLM-Pruning-And-Fairness/report/figures/pc_stsb_compare.png'),
+         plot = output,
+         width = 2100,
+         height = 1400,
+         units = "px")
+}
+
 # MNLI
 
 perf_mnli <- function(data, pruning_method_set, base_folder){
@@ -268,6 +294,7 @@ perf_all <- function(data, base_folder){
   perf_mnli(data, 'l1-unstructured', base_folder)
   perf_stsb(data, 'random-unstructured', base_folder)
   perf_mnli(data, 'random-unstructured', base_folder)
+  perf_stsb_compare(data, base_folder)
 }
 
 
