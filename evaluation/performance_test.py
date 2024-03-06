920097d2a3ff9f94cc30446f6eed255d7c32852b
Mariam Marzouk <Mariam_amir_95@hotmail.com>
2024-03-06 15:05:37 +0100

    edits to run the head_mask.npy file

--- evaluation/performance_test.py
+++ evaluation/performance_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 from performance import load_test_dataset, evaluate_metrics
 
 from transformers import (
@@ -8,7 +8,7 @@ from transformers import (
 )
 
 # load model
-model_path = '../training/final_models/MNLI/model_no2/'
+model_path = '/Users/mariamamir/TeamProject/final_models/mnli'
 
 model = RobertaForSequenceClassification.from_pretrained(
     model_path,
@@ -20,10 +20,24 @@ model = RobertaForSequenceClassification.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # load head mask
-file_path = '../results/run1/s-pruning/head_mask.npy'
+file_path = '/Users/mariamamir/TeamProject/LLM-Pruning-And-Fairness/head_mask.npy'
 head_mask = np.load(file_path, allow_pickle=True)
+
+# Replace Ellipsis with 0 (or any other appropriate value)
+head_mask[head_mask == Ellipsis] = 0
+
+# Check and convert NumPy array to numeric data
+if head_mask.dtype == object:
+    try:
+        head_mask = head_mask.astype(np.float32)
+    except ValueError as e:
+        print(f"Error converting head_mask to numeric data: {e}")
+        exit()
+
+# Convert NumPy array to PyTorch tensor
+head_mask_tensor = torch.tensor(head_mask, dtype=torch.float32)
 
 # evaluate model "performance" (not fairness)
 eval_datasets = load_test_dataset('mnli', 2)
-res_performance = evaluate_metrics(model, head_mask, tokenizer, 'mnli', eval_datasets)
+res_performance = evaluate_metrics(model, head_mask_tensor, tokenizer, 'mnli', eval_datasets)
 print(res_performance)
