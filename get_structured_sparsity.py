07e9385df30f58d7a27a4e68b169b272843e4a5e
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-13 18:45:11 +0100

    UPdate for s-pruning sparsity to work in a script

--- get_structured_sparsity.py
+++ get_structured_sparsity.py
@@ -1,9 +1,42 @@
 import numpy as np
-import transformers
+import os
+import pandas as pd
+from datetime import date
+
+from transformers import (
+    AutoTokenizer,
+    RobertaForSequenceClassification,
+    TextClassificationPipeline,
+    HfArgumentParser,
+    set_seed,
+)
+
+from dataclasses import dataclass, field, asdict
+from typing import Optional
+from pruning.utils import get_seed, get_device
+from pruning.magnitude_pruner import MagnitudePrunerOneShot
+from evaluation.performance import load_test_dataset, evaluate_metrics
 from pruning.sparsity_check import structured_sparsity
 
+@dataclass
+class ExperimentArguments:
+    """
+    Arguments needed to run the experiment
+    - seed
+    - task
+    - pruning_method
+    - sparsity_level
+    - device
+    - temperature
+    ...
+    """
+
+    run_nos: list = field(
+        metadata={"help": "Specify list of run numbers."}
+    )
+
 def single_check(run_no):
-    model = transformers.RobertaForSequenceClassification.from_pretrained(f"results/run{run_no}/pruned_model/",
+    model = RobertaForSequenceClassification.from_pretrained(f"results/run{run_no}/pruned_model/",
                                                                           use_safetensors=True, local_files_only=True)
     head_mask = np.loadtxt(f"results/run{run_no}/s-pruning/head_mask.npy")
 
@@ -15,3 +48,9 @@ def all_checks(run_no_list):
         output[i] = single_check(i)
 
     return output
+
+def main():
+    print(all_checks(run_nos))
+
+if __name__ == '__main__':
+    main()
