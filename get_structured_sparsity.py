be06105c40d06057ffb954cfa8339458ba180a01
Jana Jung <jana@dr-a-jung.de>
2024-03-13 21:47:52 +0100

    change model loading

--- get_structured_sparsity.py
+++ get_structured_sparsity.py
@@ -7,30 +7,33 @@ from transformers import (
 
 from dataclasses import dataclass, field
 from pruning.sparsity_check import structured_sparsity
+from report.utils import run_info
+
 
 @dataclass
 class ExperimentArguments:
-    """
-    Arguments needed to run the experiment
-    - seed
-    - task
-    - pruning_method
-    - sparsity_level
-    - device
-    - temperature
-    ...
-    """
-
     run_nos: list = field(
         metadata={"help": "Specify list of run numbers."}
     )
 
+
 def single_check(run_no):
-    model = RobertaForSequenceClassification.from_pretrained(f"results/run{run_no}/pruned_model/",
-                                                                          use_safetensors=True, local_files_only=True)
-    head_mask = np.loadtxt(f"results/run{run_no}/s-pruning/head_mask.npy")
+    info = run_info(run_no)
+    if info.get('task') == 'mnli':
+        model = RobertaForSequenceClassification.from_pretrained(
+            f"training/final_models/MNLI/model_no{info.get('model_no')}",
+            use_safetensors=True, local_files_only=True)
+    elif info.get('task') == 'stsb':
+        model = RobertaForSequenceClassification.from_pretrained(
+            f"training/final_models/STS-B/model_no{info.get('model_no')}",
+            use_safetensors=True, local_files_only=True)
+    else:
+        raise ValueError("task not supported")
+
+    head_mask = np.loadtxt(f"results/run{run_no}/head_mask.npy")
 
     return structured_sparsity(model, head_mask)
+
 
 def all_checks(run_no_list):
     output = {}
@@ -39,11 +42,13 @@ def all_checks(run_no_list):
 
     return output
 
+
 def main():
     parser = HfArgumentParser(ExperimentArguments)
     exp_args = parser.parse_args_into_dataclasses()[0]
 
     print(all_checks(exp_args.run_nos))
+
 
 if __name__ == '__main__':
     main()
